ca431bd965892400289319d63736c8d28a626706
cvzi <cuzi@openmail.cc>
2022-08-12 22:46:30 +0200

    revert sandbox

--- SpotifyGeniusLyrics.user.js
+++ SpotifyGeniusLyrics.user.js
@@ -13,7 +13,7 @@
 // @copyright       2020, cuzi (https://github.com/cvzi)
 // @supportURL      https://github.com/cvzi/Spotify-Genius-Lyrics-userscript/issues
 // @icon            https://avatars.githubusercontent.com/u/251374?s=200&v=4
-// @version         22.8.13
+// @version         22.8.14
 // @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
@@ -21,7 +21,6 @@
 // @grant           GM.registerMenuCommand
 // @connect         genius.com
 // @include         https://open.spotify.com/*
-// @sandbox         JavaScript
 // ==/UserScript==
 
 /*
