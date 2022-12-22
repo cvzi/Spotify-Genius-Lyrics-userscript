97b9e1bc59d32766f6a2b8649630c616441afdda
cvzi <cuzi@openmail.cc>
2022-12-22 18:18:26 +0100

    Unpin version of GeniusLyrics.js

--- SpotifyGeniusLyrics.user.js
+++ SpotifyGeniusLyrics.user.js
@@ -13,8 +13,8 @@
 // @copyright       2020, cuzi (https://github.com/cvzi)
 // @supportURL      https://github.com/cvzi/Spotify-Genius-Lyrics-userscript/issues
 // @icon            https://avatars.githubusercontent.com/u/251374?s=200&v=4
-// @version         23.1.3
-// @require https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js?version=1129650
+// @version         23.1.4
+// @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @grant           GM.xmlHttpRequest
 // @grant           GM.setValue
 // @grant           GM.getValue
