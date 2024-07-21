158b47faf759699dde23d8630938a831ffde6464
cvzi <cuzi@openmail.cc>
2024-07-21 11:27:48 +0200

    Version bump to update GeniusLyrics.js

--- SpotifyGeniusLyrics.user.js
+++ SpotifyGeniusLyrics.user.js
@@ -13,7 +13,7 @@
 // @copyright       2020, cuzi (https://github.com/cvzi)
 // @supportURL      https://github.com/cvzi/Spotify-Genius-Lyrics-userscript/issues
 // @icon            https://avatars.githubusercontent.com/u/251374?s=200&v=4
-// @version         23.6.4
+// @version         23.6.5
 // @require         https://greasyfork.org/scripts/406698-geniuslyrics/code/GeniusLyrics.js
 // @require         https://cdnjs.cloudflare.com/ajax/libs/lz-string/1.5.0/lz-string.min.js
 // @grant           GM.xmlHttpRequest
